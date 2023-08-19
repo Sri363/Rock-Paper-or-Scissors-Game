0137b150ec97a1e7208fb255ec7b12a35d202ced
Sri hatake <96491256+Sri363@users.noreply.github.com>
2023-08-19 13:48:51 +0530

    start button included and restart removed

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-let confirmation = confirm("can we play rock,paper or scissors?");
+function start() {
 
-if (confirmation) {
-    alert("let's play");
-    let playerOne = prompt("Enter rock, paper or scissors")
-    let playerChoice = playerOne.trim().toLowerCase();
-    if (playerOne) {
-        if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+    let confirmation = confirm("can we play rock,paper or scissors?");
 
-            let computer = Math.floor(Math.random() * 3 + 1);
-            if (computer === 1) {
-                computer = "rock";
-                console.log(computer);
-            } else if (computer === 2) {
-                computer = "paper";
-                console.log(computer);
+    if (confirmation) {
+        alert("let's play");
+        let playerOne = prompt("Enter rock, paper or scissors")
+        let playerChoice = playerOne.trim().toLowerCase();
+        if (playerOne) {
+            if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+
+                let computer = Math.floor(Math.random() * 3 + 1);
+                if (computer === 1) {
+                    computer = "rock";
+                    console.log(computer);
+                } else if (computer === 2) {
+                    computer = "paper";
+                    console.log(computer);
+                } else {
+                    computer = "scissors";
+                    console.log(computer);
+                }
+                if (playerChoice === computer) {
+                    alert(`Computer choose ${computer} ,its a tie!`);
+                } else if (playerChoice === "rock" && computer === "paper") {
+                    alert(`Computer choose ${computer} ,Computer wins!`);
+                } else if (playerChoice === "paper" && computer === "scissors") {
+                    alert(`Computer choose ${computer} ,Computer Wins!`);
+                } else if (playerChoice === "scissors" && computer === "rock") {
+                    alert(`Computer choose ${computer} ,Computer Wins!`);
+                }
+                else {
+                    alert(`Computer choose ${computer} ,You wins!`)
+                }
             } else {
-                computer = "scissors";
-                console.log(computer);
-            }
-            if (playerChoice === computer) {
-                alert(`Computer choose ${computer} ,its a tie!`);
-            } else if (playerChoice === "rock" && computer === "paper") {
-                alert(`Computer choose ${computer} ,Computer wins!`);
-            } else if (playerChoice === "paper" && computer === "scissors") {
-                alert(`Computer choose ${computer} ,Computer Wins!`);
-            } else if (playerChoice === "scissors" && computer === "rock") {
-                alert(`Computer choose ${computer} ,Computer Wins!`);
-            }
-            else {
-                alert(`Computer choose ${computer} ,You wins!`)
+                alert("please enter a valid choice")
             }
         } else {
-            alert("please enter a valid choice")
+            console.log(null)
         }
-    } else {
-        console.log(null)
-    }
 
-} else {
-    alert("Bye...Come again...");
+    } else {
+        alert("Bye...Come again...");
+    }
 }
